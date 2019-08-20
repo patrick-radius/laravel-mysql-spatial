92c3e927fd1e566e87fb5263fc9599a0094aac94
patrick radius <patrick@sportunity.nu>
2019-08-20 15:51:59 +0200

    Remove minimum of 2 points restriction

--- src/Types/PointCollection.php
+++ src/Types/PointCollection.php
@@ -12,10 +12,6 @@ abstract class PointCollection extends GeometryCollection
      */
     public function __construct(array $points)
     {
-        if (count($points) < 2) {
-            throw new InvalidArgumentException('$points must contain at least two entries');
-        }
-
         $validated = array_filter($points, function ($value) {
             return $value instanceof Point;
         });
